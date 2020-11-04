b89a54435e731a873144fdc080e8789230df0754
Schmoiger <32551110+Schmoiger@users.noreply.github.com>
2020-11-04 17:14:48 +0000

    Debugged test script

--- tests/test_detections.py
+++ tests/test_detections.py
@@ -7,9 +7,12 @@ import pytest
 def images_detected(scope="session"):
     images = detect_images.parse_images()
     result_json = detect_images.detect_images(images)
+    return images, result_json
 
 
 def test_valid_json(images_detected):
+    images, result_json = images_detected
+    error = False
     try:
         r = json.loads(result_json)
         error = False
@@ -19,6 +22,8 @@ def test_valid_json(images_detected):
 
 
 def test_confirm_all_processed(images_detected):
+    images, result_json = images_detected
+    error = False
     if len(images) == len(json.loads(result_json)):
         error = False
     else:
@@ -27,9 +32,10 @@ def test_confirm_all_processed(images_detected):
 
 
 def test_at_least_one_result(images_detected):
+    images, result_json = images_detected
     error = False
-    for key, value in json.loads(result_json):
-        if value == '':
+    for result in json.loads(result_json):
+        if result['detections'] == []:
             error = True
         else:
             error = error or False
