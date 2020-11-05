511d21c509980569e1126cdee32653c560c5ab97
Schmoiger <32551110+Schmoiger@users.noreply.github.com>
2020-11-05 16:52:21 +0000

    re-instate object_detection_api

--- src/object_detection.py
+++ src/object_detection.py
@@ -4,11 +4,10 @@ import torch
 import torchvision
 import torchvision.transforms as T
 import numpy as np
-'''
 # following imports are for object_detection_api only
 import matplotlib.pyplot as plt
 import cv2
-'''
+
 
 # get the pretrained model from torchvision.models
 # Note: pretrained=True will get the pretrained weights for the model.
@@ -48,7 +47,6 @@ def get_prediction(img_path, threshold):
         - image is passed through the model to get the predictions
         - class, box coordinates are obtained, but only prediction score > threshold
           are chosen.
-
     """
     img = Image.open(img_path)
     transform = T.Compose([T.ToTensor()])
@@ -62,8 +60,6 @@ def get_prediction(img_path, threshold):
     pred_class = pred_class[:pred_t+1]
     return pred_boxes, pred_class
 
-'''
-# no longer using API
 
 def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=3, text_th=3):
     """
@@ -92,4 +88,3 @@ def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=3, text_t
     plt.xticks([])
     plt.yticks([])
     plt.show()
-'''
